7d699d08e1ed8e57cc8fb0dbf51e9fd01b0985b5
Zac King <doctapper1@outlook.com>
2019-03-21 14:54:52 +0000

    Buffer class updated, Persistence class updated

--- src/buffer/Buffer.java
+++ src/buffer/Buffer.java
@@ -12,12 +12,13 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Buffer {
 
     private final Lock lock = new ReentrantLock();
-    private final Condition stepUsage = lock.newCondition();
+    private final Condition bufferUsage = lock.newCondition();
 
     private boolean bufferInUse = false;
+    private boolean finish = false;
 
     private int largestSteps = 0;
-    private String largestNumber;
+    private long largestNumber;
 
     /**
      * Set the new largest amount of steps, and the new largest number
@@ -25,10 +26,10 @@ public class Buffer {
      * @param number the actual number that was processed
      * @throws BufferError if a thread is interrupted while waiting for usage of the buffer
      */
-    public void setNewLargest(int steps, String number) throws BufferError {
+    public void setNewLargest(int steps, long number) throws BufferError {
         lock.lock();
         try {
-            if (bufferInUse) stepUsage.await();
+            if (bufferInUse) bufferUsage.await();
             bufferInUse = true;
             this.largestSteps = steps;
             this.largestNumber = number;
@@ -36,7 +37,7 @@ public class Buffer {
             throw new BufferError(e.getMessage());
         } finally {
             bufferInUse = false;
-            stepUsage.signal();
+            bufferUsage.signal();
             lock.unlock();
         }
     }
@@ -49,14 +50,14 @@ public class Buffer {
     public int getLargestSteps() throws BufferError {
         lock.lock();
         try {
-            if (bufferInUse) stepUsage.await();
+            if (bufferInUse) bufferUsage.await();
             bufferInUse = true;
             return largestSteps;
         } catch (InterruptedException e) {
             throw new BufferError(e.getMessage());
         } finally {
             bufferInUse = false;
-            stepUsage.signal();
+            bufferUsage.signal();
             lock.unlock();
         }
     }
@@ -66,11 +67,42 @@ public class Buffer {
      * this, and only when all other threads have stopped.
      * @return the largest number
      */
-    public String getLargestNumber() {
+    public long getLargestNumber() {
         lock.lock();
         try {
             return largestNumber;
         } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * See if the program is finished, doesn't really have to use any kind of thread safety measures as it doesn't
+     * really matter if the variable isn't updated for all threads instantly.
+     *
+     * @return if the program is finished
+     */
+    public boolean isFinished() {
+        return finish;
+    }
+
+    /**
+     * Set whether to tell the threads to finish up or not
+     *
+     * @param finish the variable to say if the program is finished
+     * @throws BufferError if the main thread is interrupted
+     */
+    public void setFinished(boolean finish) throws BufferError {
+        lock.lock();
+        try {
+            if (bufferInUse) bufferUsage.await();
+            bufferInUse = true;
+            this.finish = finish;
+        } catch (InterruptedException e) {
+            throw new BufferError(e.getMessage());
+        } finally {
+            bufferInUse = false;
+            bufferUsage.signal();
             lock.unlock();
         }
     }
--- src/persistence/Persistence.java
+++ src/persistence/Persistence.java
@@ -1,4 +1,65 @@
 package persistence;
 
-public class Persistence {
+import buffer.Buffer;
+import errors.BufferError;
+
+public class Persistence implements Runnable {
+
+    private static Buffer buffer = new Buffer();
+
+    public Thread t;
+    private long initialNumber;
+    private long offset;
+
+    private int steps = 0;
+    private long number;
+
+    /**
+     * A constructor for Persistence, also declares the thread to be this class
+     *
+     * @param initialNumber the initial number for this thread to check
+     * @param offset        the next number for this number to check, added each time
+     */
+    public Persistence(long initialNumber, long offset) {
+        this.t = new Thread(this);
+        this.initialNumber = initialNumber;
+        this.offset = offset;
+    }
+
+    /**
+     * Find whether or not the parameter is the next largest steps involved.
+     * <p>
+     * >> (int) Math.log10(x) + 1 is a mathematical way to get the amount of digits a number has, faster than string conversions
+     * >> x % 10 gets the last digit of a number; modding by 10 then dividing the number by 10 is faster than looping through a string conversion
+     *
+     * @param x the number to check
+     * @throws BufferError if this thread is interrupted while checking
+     */
+    private void findNextNumber(long x) throws BufferError {
+        long newNumber = 1;
+        if ((Math.log10(x) + 1) == 1) {
+            if (buffer.getLargestSteps() < steps) buffer.setNewLargest(steps, number);
+        } else {
+            while (x > 0) {
+                newNumber *= (x % 10);
+                x /= 10;
+            }
+            steps++;
+            findNextNumber(newNumber);
+        }
+    }
+
+    @Override
+    public void run() {
+        long x = initialNumber;
+        while (!buffer.isFinished()) {
+            try {
+                number = x;
+                findNextNumber(x);
+                x += offset;
+            } catch (BufferError bufferError) {
+                bufferError.printStackTrace();
+            }
+        }
+    }
 }
